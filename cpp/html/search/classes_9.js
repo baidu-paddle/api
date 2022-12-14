ade7f237b24b33d94dcef966a2e3ac6fce0d2195
jiangjiajun <jiangjiajun@baidu.com>
2022-12-14 09:33:58 +0000

    update to latest doc

--- cpp/html/search/classes_9.js
+++ cpp/html/search/classes_9.js
@@ -6,6 +6,8 @@ var searchData=
   ['paddledetpostprocessor',['PaddleDetPostprocessor',['../classfastdeploy_1_1vision_1_1detection_1_1PaddleDetPostprocessor.html',1,'fastdeploy::vision::detection']]],
   ['paddledetpreprocessor',['PaddleDetPreprocessor',['../classfastdeploy_1_1vision_1_1detection_1_1PaddleDetPreprocessor.html',1,'fastdeploy::vision::detection']]],
   ['paddlesegmodel',['PaddleSegModel',['../classfastdeploy_1_1vision_1_1segmentation_1_1PaddleSegModel.html',1,'fastdeploy::vision::segmentation']]],
+  ['paddlesegpostprocessor',['PaddleSegPostprocessor',['../classfastdeploy_1_1vision_1_1segmentation_1_1PaddleSegPostprocessor.html',1,'fastdeploy::vision::segmentation']]],
+  ['paddlesegpreprocessor',['PaddleSegPreprocessor',['../classfastdeploy_1_1vision_1_1segmentation_1_1PaddleSegPreprocessor.html',1,'fastdeploy::vision::segmentation']]],
   ['partialfc',['PartialFC',['../classfastdeploy_1_1vision_1_1faceid_1_1PartialFC.html',1,'fastdeploy::vision::faceid']]],
   ['pfld',['PFLD',['../classfastdeploy_1_1vision_1_1facealign_1_1PFLD.html',1,'fastdeploy::vision::facealign']]],
   ['pipnet',['PIPNet',['../classfastdeploy_1_1vision_1_1facealign_1_1PIPNet.html',1,'fastdeploy::vision::facealign']]],
@@ -13,5 +15,5 @@ var searchData=
   ['ppmatting',['PPMatting',['../classfastdeploy_1_1vision_1_1matting_1_1PPMatting.html',1,'fastdeploy::vision::matting']]],
   ['ppocrv2',['PPOCRv2',['../classfastdeploy_1_1pipeline_1_1PPOCRv2.html',1,'fastdeploy::pipeline']]],
   ['ppocrv3',['PPOCRv3',['../classfastdeploy_1_1pipeline_1_1PPOCRv3.html',1,'fastdeploy::pipeline']]],
-  ['pptinypose',['PPTinyPose',['../classfastdeploy_1_1vision_1_1keypointdetection_1_1PPTinyPose.html',1,'fastdeploy::vision::keypointdetection::PPTinyPose'],['../classfastdeploy_1_1pipeline_1_1PPTinyPose.html',1,'fastdeploy::pipeline::PPTinyPose']]]
+  ['pptinypose',['PPTinyPose',['../classfastdeploy_1_1pipeline_1_1PPTinyPose.html',1,'fastdeploy::pipeline::PPTinyPose'],['../classfastdeploy_1_1vision_1_1keypointdetection_1_1PPTinyPose.html',1,'fastdeploy::vision::keypointdetection::PPTinyPose']]]
 ];
--- cpp/html/search/functions_3.js
+++ cpp/html/search/functions_3.js
@@ -2,8 +2,6 @@ var searchData=
 [
   ['data',['Data',['../structfastdeploy_1_1vision_1_1Mask.html#a7712dfd53856c700e36309cf82fee23a',1,'fastdeploy::vision::Mask::Data()'],['../structfastdeploy_1_1vision_1_1Mask.html#a5b52d764bf73887aed446187c3b96f61',1,'fastdeploy::vision::Mask::Data() const']]],
   ['dbdetector',['DBDetector',['../classfastdeploy_1_1vision_1_1ocr_1_1DBDetector.html#a1ee0450a5c44d652d5a4aeb840ff7bd2',1,'fastdeploy::vision::ocr::DBDetector']]],
-  ['dbdetectorpostprocessor',['DBDetectorPostprocessor',['../classfastdeploy_1_1vision_1_1ocr_1_1DBDetectorPostprocessor.html#ab3cb609fc90b94b3d807e085c499dc98',1,'fastdeploy::vision::ocr::DBDetectorPostprocessor']]],
-  ['dbdetectorpreprocessor',['DBDetectorPreprocessor',['../classfastdeploy_1_1vision_1_1ocr_1_1DBDetectorPreprocessor.html#a8de29ed8fb4040f6ef44aece798ab132',1,'fastdeploy::vision::ocr::DBDetectorPreprocessor']]],
   ['deletepaddlebackendpass',['DeletePaddleBackendPass',['../structfastdeploy_1_1RuntimeOption.html#a18e659f82dc902f4f4fade0ce8492ae3',1,'fastdeploy::RuntimeOption']]],
   ['detectionresult',['DetectionResult',['../structfastdeploy_1_1vision_1_1DetectionResult.html#a9bf2936623e1e016c7f332eefeffc171',1,'fastdeploy::vision::DetectionResult']]],
   ['disableflycv',['DisableFlyCV',['../namespacefastdeploy_1_1vision.html#aa83d1b5a8d474d89d936e8d9927878cc',1,'fastdeploy::vision']]],
@@ -12,6 +10,7 @@ var searchData=
   ['disablenormalizeandpermute',['DisableNormalizeAndPermute',['../classfastdeploy_1_1vision_1_1facedet_1_1SCRFD.html#a0f6843655425e91aff9331175192b109',1,'fastdeploy::vision::facedet::SCRFD']]],
   ['disablepaddleloginfo',['DisablePaddleLogInfo',['../structfastdeploy_1_1RuntimeOption.html#a6b66505a53c379b42da0844adde4f76f',1,'fastdeploy::RuntimeOption']]],
   ['disablepaddletrtcollectshape',['DisablePaddleTrtCollectShape',['../structfastdeploy_1_1RuntimeOption.html#a91232808106866c55453f31a2dcfeccf',1,'fastdeploy::RuntimeOption']]],
+  ['disablepaddletrtops',['DisablePaddleTrtOPs',['../structfastdeploy_1_1RuntimeOption.html#a709d3b1ddfe6e34146de8ffc241023a7',1,'fastdeploy::RuntimeOption']]],
   ['disablepinnedmemory',['DisablePinnedMemory',['../structfastdeploy_1_1RuntimeOption.html#ad42ba8fea3322838160a49b8b90a18df',1,'fastdeploy::RuntimeOption']]],
   ['disablerecordtimeofruntime',['DisableRecordTimeOfRuntime',['../classfastdeploy_1_1FastDeployModel.html#a7c1586af5d28d109ab7e73138e6549aa',1,'fastdeploy::FastDeployModel']]],
   ['disabletrtfp16',['DisableTrtFP16',['../structfastdeploy_1_1RuntimeOption.html#a1267e51560c00a54d5f9c1ae8d0dc36b',1,'fastdeploy::RuntimeOption']]]
